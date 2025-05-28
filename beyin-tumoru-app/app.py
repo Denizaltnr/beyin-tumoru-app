f09572c5dcaab6e2bc8666aeb4384ea38ec9c47a
Denizaltnr <skinsbetscom27@gmail.com>
2025-05-28 05:41:01 +0300

    Delete beyin-tumoru-app directory

--- beyin-tumoru-app/app.py
+++
@@ -1,8 +0,0 @@
-import os
-import streamlit as st
-
-if not os.path.exists("svm_model.pkl"):
-    import download_models
-
-st.title("🧠 Beyin Tümörü Sınıflandırma Uygulaması")
-st.write("Model dosyaları başarıyla yüklendiyse, test görselinizi yükleyerek tahmin alabilirsiniz.")
